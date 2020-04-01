7c3ede80ad58bf5f999a1cce730c0752bb7773d5
homdev <homdev44@gmail.com>
2020-04-01 02:32:00 +0200

    first commit

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,15 @@
+class SurveysController < ApplicationController
+
+
+    def index
+       @total_score = sum_score
+       render :json => {totale_score: @total_score}
+
+    end
+
+    def sum_score
+        params[:q1].to_i + params[:q2].to_i + params[:q3].to_i
+    end
+
+
+end
---
+++ app/models/survey.rb
@@ -0,0 +1,4 @@
+class Survey < ApplicationRecord
+
+
+end
